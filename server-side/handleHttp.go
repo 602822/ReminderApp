b9dc1185ce12a0c3a6d98b86f85c3c6be96a9d36
602822 <89077126+602822@users.noreply.github.com>
2023-12-29 14:21:03 +0100

    event retrieved from database

--- server-side/handleHttp.go
+++ server-side/handleHttp.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -25,31 +26,55 @@ type User struct {
 var client *mongo.Client
 var DBError error
 var userId primitive.ObjectID
-var newUser bool = false
+var newUser bool = true
+
+func createNewUser() {
+	id := primitive.NewObjectID()
+	document := User{
+		ID: id,
+	}
+	collection := client.Database("MyMongoDB").Collection("Users")
+	_, err := collection.InsertOne(context.TODO(), document)
+
+	if err != nil {
+		fmt.Println("Error inserting User")
+	}
+	fmt.Println("User with Id :", id.String(), "inserted successfully")
+	newUser = false
+	userId = id
+}
+
+func retrieveUserEvents() {
+	fmt.Println("UserId: ", userId)
+	var events []bson.M
+	context := context.TODO()
+	eventCollection := client.Database("MyMongoDB").Collection("Events")
+	filter, err := eventCollection.Find(context, bson.M{"_id": userId})
+
+	if err != nil {
+		fmt.Println("Error finding document in collection: ", err)
+	}
+
+	if err = filter.All(context, &events); err != nil {
+		fmt.Println("Error decoding document into result: ", err)
+	}
+	fmt.Println("Events: ", events)
+}
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-
-	if newUser == false {
-		id := primitive.NewObjectID()
-		document := User{
-			ID: id,
-		}
-		collection := client.Database("MyMongoDB").Collection("Users")
-		_, err := collection.InsertOne(context.TODO(), document)
-
-		if err != nil {
-			fmt.Println("Error inserting User")
-		}
-		fmt.Println("User with Id :", id.String(), "inserted successfully")
-		newUser = true
-		userId = id
+	fmt.Println("You entered the main page")
+	if newUser == true {
+		createNewUser()
 	}
+
+	retrieveUserEvents()
+
 	filepath := filepath.Join("client-side", "html", "index.html")
 	http.ServeFile(w, r, filepath)
 }
 
 func newEventPage(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("You entered the create new event page")
 	if r.Method == http.MethodPost {
 		handleFormSubmit(w, r)
 	}
